d29a81ea3b24c4939261ae8d3c1c4f9c04912741
yopse <82827725+yopse@users.noreply.github.com>
2022-10-01 19:11:23 +0530

    Navbar is added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen ">
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+            Goggl 🔎
+          </p>
+        </Link>
+
+        <button
+          type="button"
+          onClick={() => setDarkTheme(!darkTheme)}
+          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white rounded-full px-4 py-2 hover:shadow-lg"
+        >
+          {darkTheme ? "Light 💡" : "Dark 🌙"}
+        </button>
+      </div>
     </div>
   );
 };
