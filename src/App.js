40d4989c3ea147478e76efb3dab00751d59aaa9a
yopse <82827725+yopse@users.noreply.github.com>
2022-10-01 17:11:13 +0530

    basic setup is done Navbar Footer Routes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Routes from "./components/Routes";
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
